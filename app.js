9b9601db3a487abbf9b6a074bc501f9d71401f6e
Feruzabonu <feruzabonu12@gmail.com>
2021-08-31 01:59:47 +0500

    imtixon-1

--- app.js
+++ app.js
@@ -34,15 +34,25 @@ let DATA = {
 }
 
 
-// ========================== additional elements ===========================
+                         // ========================== additional elements ===========================
 
 // ===================get Elements=================
 
 let Icons = document.querySelectorAll("i");
 let userInfo = document.querySelector(".user-info");
 let chat = document.querySelector(".chat");
+let person = document.querySelector(".person");
+let userInfModal = document.querySelector(".user-info-modal");
+let upDownUl = document.querySelector(".up-down-ul");
+let upDown = document.querySelector(".up-down");
+let hidden = document.querySelector(".hidden");
+let settingLi = document.querySelector(".setting_li");
+let settings = document.querySelector(".settings");
+let userslistNav = document.querySelector(".users-list-nav");
+let humburger = document.querySelector(".humburger");
 
-// ================open user Info=================
+
+// ================Open user Info=================
 
 for (let el of Icons){
    let check = false;
@@ -55,30 +65,75 @@ for (let el of Icons){
        userInfo.style.display = "inline-block";
        check = true;
        el.style.color = "#168ACD";
-      
     }
     else{
         userInfo.style.display = "none";
         check = false;
         el.style.color = "#A8A8A8";
      }
+     if(check){
+        for(let el of Icons){
+            el.addEventListener("click", function(e){
+                if((el.className == "fas fa-times")){
+                    userInfo.style.display = "none";
+                    for(let el of Icons){
+                        el.className === "fas fa-columns"?el.style.color = "#A8A8A8":""
+                     }
+                     check = false;
+                 }
+            })
+        }
+    }
      
    }) 
 }
 
+// ================Open user Info Modal=================
+
+person.addEventListener("click",function(e){
+    userInfModal.style.display = "flex"
+})
 for(let el of Icons){
     el.addEventListener("click", function(e){
         if((el.className == "fas fa-times")){
-            userInfo.style.display = "none";
-            
-            for(let el of Icons){
-                el.className === "fas fa-columns"?el.style.color = "#A8A8A8":""
-             }
-             check = false;
+           setTimeout(function(){userInfModal.style.display = "none"},100)
          }
     })
 }
 
+// ================== Open Humburgar Modal ===============
 
+console.log(userslistNav.firstElementChild)
+userslistNav.firstElementChild.addEventListener("click", function(e){
+    humburger.style.display = "flex"
+})
 
+upDownUl.addEventListener("click", function(e){
+    upDown.firstChild.style.backgroundColor = "none";
+    if(upDown.firstChild.className === "fas fa-chevron-down"){
+        upDown.firstChild.className = "fas fa-chevron-up";
+        hidden.style.display = "inline-block"
+    }else{
+        upDown.firstChild.className = "fas fa-chevron-down";
+        hidden.style.display = "none"
+    }
+    upDown.firstChild.addEventListener("click",function(e){
+        upDown.firstChild.style.backgroundColor = "none"
+        console.log(e.target.style.backgroundColor)
+    })
 
+})
+
+// ================== Open Settings Modal ===============
+
+settingLi.addEventListener("click", function(e){
+    humburger.style.display = "none";
+    settings.style.display = "flex"
+})
+for(let el of Icons){
+    el.addEventListener("click", function(e){
+        if((el.className == "fas fa-times")){
+           setTimeout(function(){settings.style.display = "none"},100)
+         }
+    })
+}
